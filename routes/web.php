b65b490dcd99df314cbc31a845b69e14f9fb7bad
Saviskar <saviskar2003@gmail.com>
2026-01-29 15:40:31 +0530

    feat: add protected routes and rba

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,19 @@ use App\Livewire\CheckoutPage;
 use App\Livewire\MyOrdersPage;
 use App\Livewire\OrderDetailPage;
 
-Route::get('/', HomePage::class)->name('home');
-Route::get('/shop/{type?}', ShopPage::class)->name('shop');
-Route::get('/product/{product}', ProductDetail::class)->name('product.detail');
-Route::get('/cart', CartPage::class)->name('cart');
-Route::get('/checkout', CheckoutPage::class)->name('checkout');
-Route::get('/checkout/success', CheckoutPage::class)->name('checkout.success');
-Route::get('/checkout/cancel', CheckoutPage::class)->name('checkout.cancel');
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    Route::get('/', HomePage::class)->name('home');
+    Route::get('/shop/{type?}', ShopPage::class)->name('shop');
+    Route::get('/product/{product}', ProductDetail::class)->name('product.detail');
+    Route::get('/cart', CartPage::class)->name('cart');
+    Route::get('/checkout', CheckoutPage::class)->name('checkout');
+    Route::get('/checkout/success', CheckoutPage::class)->name('checkout.success');
+    Route::get('/checkout/cancel', CheckoutPage::class)->name('checkout.cancel');
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
